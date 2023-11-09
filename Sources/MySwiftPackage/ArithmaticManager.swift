91ac104c6e2c0acc012647e560fc3ce33de2aa29
Lakshmi Narayanan N <lara1435@gmail.com>
2023-11-09 16:37:08 +0530

    Initial commit

---
+++ Sources/MySwiftPackage/ArithmaticManager.swift
@@ -0,0 +1,19 @@
+//
+//  File.swift
+//  
+//
+//  Created by Lakshmi Narayanan N on 09/11/23.
+//
+
+import Foundation
+
+public class ArithmaticManager {
+    
+    public func add<T: Numeric>(first: T, second: T) -> T {
+        first + second
+    }
+    
+    public func subtract<T: Numeric>(first: T, second: T) -> T {
+        first - second
+    }
+}
